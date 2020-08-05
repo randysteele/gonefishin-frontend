b071c626bcc2e1670edf62fa3410dcc2e3c749e2
steelerx155 <steelerx155@gmail.com>
2020-08-05 14:53:27 -0400

    coded features controller

---
+++ .history/gone-fishin-backend/app/controllers/api/v1/features_controller_20200805144425.rb
@@ -0,0 +1,7 @@
+class Api::V1::FeaturesController < ApplicationController
+
+    def index
+        features = Feature.all
+        render json: FeatureSerializer.new(features) 
+    end
+end
--- .history/gone-fishin-backend/app/controllers/api/v1/features_controller_20200805144736.rb
+++ .history/gone-fishin-backend/app/controllers/api/v1/features_controller_20200805144736.rb
@@ -19,21 +19,4 @@ class Api::V1::FeaturesController < ApplicationController
     end
 end
 
-def destroy 
-    feature = Feature.find(params["id"])
-    destination = destination.find(feature.destination_id)
-    feature.destory
-    render json: destination
-end
-
-private 
-
-def set_destination
-    destination = Destination.find([params[:destination_id]])
-end
-
-def feature_params
-    params.require(:feature).permit(:name, :destination_id)
-end
-
 end
