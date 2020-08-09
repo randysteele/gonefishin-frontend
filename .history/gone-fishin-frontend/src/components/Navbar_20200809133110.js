188b4321cfe935d73aef1fe6783724ef4f4c200c
steelerx155 <steelerx155@gmail.com>
2020-08-09 13:32:02 -0400

    added Navbar component

---
+++ .history/gone-fishin-frontend/src/components/Navbar_20200809133110.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Navbar = (props) => {
+
+    return (
+        <div>
+            <Link to='/destinations' style={{paddingRight: '10px'}}>Destinations</Link>
+            <Link to='/destinations/new'>Add Destination!</Link>
+        </div>
+    )
+}
