14f271b58c0442ae5956d28509e96dff706d9a3d
steelerx155 <steelerx155@gmail.com>
2021-01-20 19:44:23 -0500

    added deleteDestination action

---
+++ .history/gone-fishin-frontend/src/components/DestinationsList_20210120193253.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+
+
+
+
+const DestinationsList = (props) => {
+
+    const handleDeleteDestination = (destination) => {
+        props.deleteDestination(destination.id) 
+    }
+
+    
+    
+    return (
+       <div>
+       <h2> Fishing Lake List:</h2> {props.destinations.reverse().map(dest => <h5><div key={dest.id}>Lake Name:<Link to={`/destinations/${dest.id}`}><br></br>{dest.name}</Link>  <br></br> City: {dest.city} <br></br> State: {dest.state} <br></br> Description: {dest.description} handleDeleteDestination= {handleDeleteDestination}</div></h5>)} 
+       <button type="button" onClick={() => handleRemoveDestination(destination)}></button>
+   </div>
+   
+ 
+    )
+}
+
+export default connect(null, {deleteDestination}) (DestinationsList)
---
+++ gone-fishin-frontend/src/actions/deleteDestination.js
@@ -0,0 +1,10 @@
+export const deleteDestination = (destinationId) => {
+
+    return (dispatch) => {
+       return fetch(`http:localhost:3000/api/v1/destinations/${destinationId}`, { 
+        method: 'DELETE'       
+    })
+     .then(response => response.json())
+     .then(destination => dispatch({type: 'DELETE_DESTINATION', payload: destination}))
+  }
+}
